d5bed74b6fbed196c83ed131cfeb62bff6e14cfb
Pham Quang Huy <quanghuy.h20@gmail.com>
2021-03-24 16:08:55 +0700

    initial commit

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const app = require('express')()
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
 const PORT = process.env.PORT || 3000
 
-app.get("", (req, res) => {
-    res.send("/public/index.html")
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+    res.render("home")
 })
 
 app.listen(PORT, ()=>{
